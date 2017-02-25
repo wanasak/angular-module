78b44c4a5f7fd23e2b81ab18955c25a5878f5c0b
Wanasak <u510610433@gmail.com>
2017-02-25 20:51:38 +0700

    refractor app module v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 import { TitleComponent } from './title.component';
@@ -12,7 +11,6 @@ import { ContactModule } from './contact/contact.module';
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule,
     ContactModule
   ],
   declarations: [
